082c89e7922fff1bd062a64df5b71e11d62e07c2
Yogesh Manjhi <yogeshmanjhi93@gmail.com>
2019-09-14 13:04:46 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -4,8 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://localhost:27017/blog_everyone',
   cookieKey: '123123123',
-  accessKeyId: 'AKIAQ4LP2Y6LSTGGBLHA',
-  secretAccessKey: '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'
+
 
 };
   
